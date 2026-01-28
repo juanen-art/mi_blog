07d4ca1dc7bfc659a280f2dff0db2b7aa1276484
juanen <Juanenriquefg11@gmail.com>
2026-01-28 20:04:53 +0100

    Etapa5: personalización del script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Mensaje que aparece en la consola cuando se carga la página
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Página cargada correctamente.');
+  console.log('Página cargada correctamente. BIENVENIDOOOOS');
 });
